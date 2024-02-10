ba12deb4092797a6327f5634fa1e3ca31b14f919
Yisus <jesusgameroom@gmail.com>
2024-02-10 15:19:18 -0400

    cproj tool base

--- src/tools/cproj.c
+++ src/tools/cproj.c
@@ -4,7 +4,32 @@
  * cproj.c : CLI util that generates C projects.
  */
 
-int main(void) {
-    // TODO: CProj.
+#include "types.h"
+#include <stdio.h>
+
+#define eprintf(format, ...) fprintf(stderr, format, __VA_ARGS__)
+
+// TODO: Define this with CMake.
+#ifndef CLANG_FORMAT_SOURCE
+#warning No .clang-format source found.
+#define CLANG_FORMAT_SOURCE "# Missing source."
+#endif
+
+// TODO: Define this with CMake.
+#ifndef CMAKELISTS_SOURCE
+#warning No CMakeLists.txt source found.
+#define CMAKELISTS_SOURCE "# Missing source."
+#endif
+
+i32 main(i32 argc, char **argv)
+{
+    if (argc < 2) {
+        eprintf("cproj error: no metadata provided.\n");
+        eprintf("\nusage is: cproj.exe <project name> [path]\n");
+        return -1;
+    }
+
+    /// TODO: Create project.
+
     return 0;
 }
