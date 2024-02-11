ac32798d9a36e81fa6cad81170b385c42ecc2400
Yisus <jesusgameroom@gmail.com>
2024-02-10 22:55:56 -0400

    added useful utils

---
+++ src/util.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2024 Jesús "YisusGaming" Blanco
+ *
+ * util.h : Useful utilities.
+ */
+
+#ifndef YISUS_UTIL_H
+#define YISUS_UTIL_H
+
+#include "types.h"
+#include <stdio.h>
+
+#define eprintf(f, ...) fprintf(stderr, f, __VA_ARGS__)
+#define failifnull(with, check, msg, ...)                                      \
+    if (check == null) {                                                       \
+        eprintf(msg, __VA_ARGS__);                                             \
+        return with;                                                           \
+    }
+#define retifnull(check, msg, ...)                                             \
+    if (check == null) {                                                       \
+        eprintf(msg, __VA_ARGS__);                                             \
+        return;                                                                \
+    }
+#define errifnull(check, msg, ...)                                             \
+    if (check == null) {                                                       \
+        eprintf(msg, __VA_ARGS__);                                             \
+    }
+
+#endif //! YISUS_UTIL_H
